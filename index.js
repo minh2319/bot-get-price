21d7d97c454af2907bb14b9ccd26c2b5ece854b1
minhhg <minh2319@gmail.com>
2022-08-21 23:11:19 +0700

    update

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ async function main() {
         token: '2036073407:AAF1Q-PP1EuAsg73GKRWw-cBg-QbW5Y7pZk',
         chatId: '-565852162'
     });
-    const timeOut1=10000;
-    const timeOut2=20000;
-    const timeOut3=30000;
+    const timeOut1=11000;
+    const timeOut2=13000;
+    const timeOut3=15000;
 
     const httpsAgent = new https.Agent({
         rejectUnauthorized: false, // (NOTE: this will disable client verification)
     })
-    excute(httpsAgent,axios,cheerio,arrDienMayXanh,timeOut1,notify,'.kmbox.kb2.active','strong')
+    excute2(axios,cheerio,arrDienMayXanh,timeOut1,notify,'.kmbox.kb2.active','strong')
     excute(httpsAgent,axios,cheerio,arrDienChoLon,timeOut2,notify,'.price_block','strong')
     excute(httpsAgent,axios,cheerio,arrNguyenKim,timeOut3,notify,'.product_info_price_value-final','span')
 }
@@ -39,6 +39,30 @@ async function main() {
 
         arrDienMayXanh.forEach((value,index) => {
              axios.get(value, { httpsAgent })
+                .then(function (response) {
+                    console.log(response);
+                    const $ = cheerio.load(response?.data); // load HTML
+                    const job = $(className).find(tag).first().text(); // lấy tên job, được nằm trong thẻ a < .job__list-item-title
+                    const text = value + ' : '+job;
+                    if(arrDienMayXanhValue[value]!=text) {
+                        const customText='Old : '+ arrDienMayXanhValue[value]+ '\n<b> New : '+text +'</b>'
+                        notify.send(customText,{},{parse_mode:'html'});
+                        arrDienMayXanhValue[value]=text
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+    }, timeOut);
+}
+async function excute2(axios,cheerio,arrDienMayXanh,timeOut,notify,className,tag) {
+    const arrDienMayXanhValue=[];
+
+    setInterval(function () {
+
+        arrDienMayXanh.forEach((value,index) => {
+            axios.get(value)
                 .then(function (response) {
                     console.log(response);
                     const $ = cheerio.load(response?.data); // load HTML
